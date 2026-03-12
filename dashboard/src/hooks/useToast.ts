d45b95d700b3a3cf99f88514281625fe4f34b2b0
Vlad Tkachuk <tkvlads@gmail.com>
2026-03-12 13:02:10 +0200

    feat: added useAuth and useToast hooks

---
+++ dashboard/src/hooks/useToast.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react'
+import { ToastContext } from '../components/ui/Toast'
+
+export function useToast() {
+  const ctx = useContext(ToastContext)
+  if (!ctx) {
+    throw new Error('useToast must be used within <ToastProvider>')
+  }
+  return { toast: ctx }
+}
